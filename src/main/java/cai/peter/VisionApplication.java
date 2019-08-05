91cdbb9b8efad7ae52b30f2118224ecb2721ae9e
Peter Cai <petercaica@hotmail.com>
2019-08-04 22:54:33 -0400

    entities

---
+++ src/main/java/cai/peter/VisionApplication.java
@@ -0,0 +1,17 @@
+package cai.peter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = "cai.peter.vision.persistence")
+@ImportResource("classpath*:applicationContext.xml")
+public class VisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisionApplication.class, args);
+	}
+
+}
