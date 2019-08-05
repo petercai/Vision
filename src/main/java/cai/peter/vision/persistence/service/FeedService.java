6f5c8deff33ce20c193d1a7afc9ef60450b50389
Peter Cai <petercaica@hotmail.com>
2019-08-05 14:46:46 -0400

    FeedService.java

---
+++ src/main/java/cai/peter/vision/persistence/service/FeedService.java
@@ -0,0 +1,67 @@
+package cai.peter.vision.persistence.service;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.Set;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.io.IOUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import cai.peter.vision.favicon.AbstractFaviconFetcher;
+import cai.peter.vision.favicon.AbstractFaviconFetcher.Favicon;
+import cai.peter.vision.feed.FeedUtils;
+import cai.peter.vision.persistence.entity.Feed;
+import cai.peter.vision.persistence.repository.FeedsRepository;
+
+@Service
+@Transactional
+public class FeedService {
+
+	private final FeedsRepository feedDAO;
+	private final Set<AbstractFaviconFetcher> faviconFetchers;
+
+	private Favicon defaultFavicon;
+
+	public FeedService(FeedsRepository feedDAO, Set<AbstractFaviconFetcher> faviconFetchers) {
+		this.feedDAO = feedDAO;
+		this.faviconFetchers = faviconFetchers;
+
+		try {
+			defaultFavicon = new Favicon(IOUtils.toByteArray(getClass().getResource("/images/default_favicon.gif")), "image/gif");
+		} catch (IOException e) {
+			throw new RuntimeException("could not load default favicon", e);
+		}
+	}
+
+	public synchronized Feed findOrCreate(String url) {
+		String normalized = FeedUtils.normalizeURL(url);
+		Feed feed = feedDAO.findByUrl(normalized);
+		if (feed == null) {
+			feed = new Feed();
+			feed.setUrl(url);
+			feed.setNormalizedUrl(normalized);
+			feed.setNormalizedUrlHash(DigestUtils.sha1Hex(normalized));
+			feed.setDisabledUntil(new Date(0));
+			feedDAO.save(feed);
+		}
+		return feed;
+	}
+
+	public Favicon fetchFavicon(Feed feed) {
+
+		Favicon icon = null;
+		for (AbstractFaviconFetcher faviconFetcher : faviconFetchers) {
+			icon = faviconFetcher.fetch(feed);
+			if (icon != null) {
+				break;
+			}
+		}
+		if (icon == null) {
+			icon = defaultFavicon;
+		}
+		return icon;
+	}
+
+}
